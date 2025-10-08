23f17fdb4bcb7ac9e01ca85753ad5ec23a864f79
Luan Dias de Ávila <luandias1465@gmail.com>
2025-10-08 14:35:07 -0300

    Corrige serializador de imagem para URL relativo

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Project
-        fields = ['id', 'title', 'description', 'tags', 'link_github', 'link_demo', 'image', 'order']
+        fields = ['id', 'title', 'description', 'tags', 'link_github', 'link_demo', 'image_url', 'order']
+
+    def get_image_url(self, obj):
+        if obj.image and hasattr(obj.image, 'url'):
+            return obj.image.url
+        return None
